05f6bd0fe8a5ef3fb0badfd6f2497c87a2a01876
Night shift <shem.b.cola@gmail.com>
2025-12-31 11:52:50 +0300

    Final TWA configuration and assetlinks

---
+++ app/src/main/java/com/shemcola/shoppingyako/DelegationService.java
@@ -0,0 +1,22 @@
+package com.shemcola.shoppingyako;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
